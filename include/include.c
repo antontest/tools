a78cf7d5f97efe8767af5a471a5a6f73484587ce
Anton <anton@anton-laptop.(none)>
2015-09-26 13:34:22 +0800

    add new lib

---
+++ include/include.c
@@ -0,0 +1,21 @@
+/**
+ * usual head files
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/*********************************************************
+ **************    Function Declaration    ***************
+ *********************************************************/
+
+/*********************************************************
+ ******************    Main Function    ******************
+ *********************************************************/
+int main(int agrc, char *agrv[])
+{
+    int rt = 0; /* return value of function main */
+
+    return rt;
+}
